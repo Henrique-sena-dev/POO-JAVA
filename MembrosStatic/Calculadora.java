828448c7819fd0954bffc5730abaf80af73ffc4f
Henrique <henriquesenah055@gmail.com>
2026-08-07 18:22:31 -0300

    exercicios membros staticos

---
+++ MembrosStatic/Calculadora.java
@@ -0,0 +1,14 @@
+package MembrosStatic;
+
+public class Calculadora {
+
+    public static int somar(int a,int b){
+         return a + b;
+    }
+    public static int subtrair(int a, int b){
+        return a - b;
+    }
+    public static int multi(int a , int b){
+        return a * b;
+    }
+}
---
+++ MembrosStatic/Main.java
@@ -0,0 +1,10 @@
+package MembrosStatic;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(Calculadora.subtrair(11,10));
+        System.out.println(Calculadora.somar(5,5));    
+        System.out.println(Calculadora.multi(2,4));
+    }
+}
