d274729fa3485692d3109929f7d48d9855cc7580
Henrique <henriquesenah055@gmail.com>
2026-08-12 20:52:38 -0300

    array

--- Agregacao/Universidade/Main.java
+++ Agregacao/Universidade/Main.java
@@ -3,12 +3,13 @@ package Agregacao.Universidade;
 public class Main {
     
     public static void main(String[] args) {
+        Aluno[] array = new Aluno[5];
         Professor p1 = new Professor("Jose", "Matemática");
-        Aluno a1 = new Aluno("Lelo", 26);
-        Curso c1 = new Curso("Matematica", 3,p1,a1);
-        c1.setAluno(a1);
+        array [0] = new Aluno("Lelo", 26);
+        Curso c1 = new Curso("Matematica", 3,p1,array[0]);
+        c1.setAluno(array[0]);
         c1.setProfessor(p1);
-        System.out.println(c1);
+        System.out.println(array[0]);
 
 
     }
